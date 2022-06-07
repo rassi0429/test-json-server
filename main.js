9a0372b0f925b19649ad7b08243b6616f36aee10
rassi0429 <taitokokoa+rassi@gmail.com>
2022-06-08 07:08:25 +0900

    redirect

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const path = require("path")
 app.listen(3082, () => console.log("ok"))
 
 app.get("*.json", (req, res) => {
+    console.log("access json")
     res.sendFile(path.join(__dirname, "./Box.7zbson"))
 })
 
 app.get("*", (req, res) => {
+    console.log("access anonimous")
     res.redirect('/foo.json')
 })
