42065b7dd5734a61b280b2aba19a552c83dc4ce1
rassi0429 <taitokokoa+rassi@gmail.com>
2022-06-08 07:07:24 +0900

    redirect

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const app = express()
 const path = require("path")
 app.listen(3082, () => console.log("ok"))
 
-app.get("*", (req, res) => {
+app.get("*.json", (req, res) => {
     res.sendFile(path.join(__dirname, "./Box.7zbson"))
+})
+
+app.get("*", (req, res) => {
+    res.redirect('/foo.json')
 })
